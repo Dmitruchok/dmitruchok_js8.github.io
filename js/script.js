852f8ec7dee3248485d796dc3e2ecb3e59f2950b
Andrei Dmytruk <mallboroks@mail.ru>
2016-09-27 16:18:29 +0300

    8

--- js/script.js
+++ js/script.js
@@ -23,29 +23,4 @@ $(function () {
     $tab_2.css( 'display', 'none' ),
     $tab_3.css( 'display', 'none');
   });
-
-  $twoList.on('click', function () {
-    $(this).css( 'background', '#007fff' ),
-    $tab_2.css( 'display', 'block' ),
-    $wrapper.css( 'overflow', 'none' ),
-    $container.css( 'margin', '10px 0px 10px 10px' ),
-    $oneList.css( 'background', '#fff' ),
-    $threeList.css( 'background', '#fff' ),
-    $tab_1.css( 'display', 'none' ),
-    $tab_3.css( 'display', 'none' );
-  });
-
-  $threeList.on('click', function () {
-    $(this).css( 'background', '#007fff');
-    $tab_3.css({
-      display: 'block',
-      height: 'auto'
-    }),
-    $wrapper.css( 'overflow', 'auto' ),
-    $container.css( 'margin', '10px 0 10px 10px' ),
-    $oneList.css( 'background', '#fff' ),
-    $twoList.css( 'background', '#fff'),
-    $tab_1.css( 'display', 'none' ),
-    $tab_2.css( 'display', 'none' );
-  });
 });
